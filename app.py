43e56b0a3b9de540133b897ab17a9f944bb245fe
Asylhum74 <asylhum74@gmail.com>
2026-06-12 08:27:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -344,7 +344,6 @@ def widget():
 @app.route('/admin')
 def admin():
     return send_from_directory('.', 'admin.html')
-
+    
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
